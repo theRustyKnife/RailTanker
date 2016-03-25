f738de96bdc7bb14c0ebaa4606753fe9a4ca1cae
Choumiko <ghostface@ghostface.info>
2016-03-25 22:23:43 +0100

    remove dependency for SqueakThrough, fix pump collision in data-final-fixes instead

---
+++ data-final-fixes.lua
@@ -0,0 +1,3 @@
+if data.raw["pump"]["small-pump"] then
+  data.raw["pump"]["small-pump"].collision_box = {{-0.29, -0.29}, {0.29, 0.29}}
+end
--- data.lua
+++ data.lua
@@ -9,7 +9,3 @@ require("prototypes.recipe.recipes")
 
 --tech
 require("prototypes.tech.tech")
-
-if data.raw["pump"]["small-pump"] then
-  data.raw["pump"]["small-pump"].collision_box = {{-0.29, -0.29}, {0.29, 0.29}}
-end
